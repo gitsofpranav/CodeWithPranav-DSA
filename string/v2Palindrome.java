61b4de041cd9f9e47b07c31887f848fd73f3f000
gitsofpranav <dewanganpranav2@gmail.com>
2025-05-28 19:33:50 +0530

    check palindrome version2 Optimized code

--- string/v2Palindrome.java
+++ string/v2Palindrome.java
@@ -18,10 +18,39 @@ public class v2Palindrome {
         }
         return true;
     }
+
+
+    // optimized
+    public static boolean isPalindromeVersion2optimized(String s){
+        int left = 0;
+        int right = s.length()-1;
+        while(left < right){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+
+            char l = Character.toLowerCase(s.charAt(left));
+            char r = Character.toLowerCase(s.charAt(right));
+
+            if(l != r){
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
     public static void main(String[] args) {
         String s = "racer a car";
         String s2 = "A man, a plan, a canal: Panama";
         System.out.println(isPalindromeVersion2(s));
         System.out.println(isPalindromeVersion2(s2));
+        System.out.println(isPalindromeVersion2optimized(s2));
     }
 }
