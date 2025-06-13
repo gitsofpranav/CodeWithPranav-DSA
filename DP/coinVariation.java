3b0393a3c468f5d5bdfae8dab81f9124251f34f0
gitsofpranav <dewanganpranav2@gmail.com>
2025-06-13 15:46:50 +0530

    Coin Change II  Dp problem find minimum number of coin

---
+++ DP/coinVariation.java
@@ -0,0 +1,38 @@
+public class coinVariation {
+    static int [][]dp;
+    public static int coin2(int []coin, int sum){
+        int n = coin.length;
+        dp = new int[n+1][sum+1];
+        for(int i=0; i<n+1; i++){
+            for(int  j=0; j<sum+1; j++){
+                if(i==0){
+                    dp[i][j] = Integer.MAX_VALUE-1;
+                }
+                if(j==0){
+                    dp[i][j] = 0;
+                }
+
+            }
+        }
+
+
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<sum+1; j++){
+               if(coin[i-1] <= j) {
+                dp[i][j] = Math.min(dp[i][j-coin[i-1]]+1, dp[i-1][j]);
+               }else {
+                dp[i][j] = dp[i-1][j];
+              }
+            }
+        }
+
+        return dp[n][sum] == Integer.MAX_VALUE - 1 ? -1 : dp[n][sum];
+
+
+    }
+    public static void main(String[] args) {
+        int []coin = {1,2,3};
+        int sum = 5;
+        System.out.println(coin2(coin, sum));
+    }
+}
